6264bcc758bbe5e639010e3c134a30fc952b6719
achujelas123 <achujel3@gmail.com>
2017-04-07 20:21:37 +0300

    testing Single Table Strategy

--- src/com/hibernate/test/vehicle/TwoWheeler.java
+++ src/com/hibernate/test/vehicle/TwoWheeler.java
@@ -1,8 +1,10 @@
 package com.hibernate.test.vehicle;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("Bike")
 public class TwoWheeler extends Vehicle {
 
     private String steeringHandle;
--- src/com/hibernate/test/vehicle/Vehicle.java
+++ src/com/hibernate/test/vehicle/Vehicle.java
@@ -2,14 +2,23 @@ package com.hibernate.test.vehicle;
 
 import com.hibernate.test.user.UserDetails;
 
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.ManyToMany;
 import java.util.ArrayList;
 import java.util.Collection;
 
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(
+        name = "VEHICLE_TYPE",
+        discriminatorType = DiscriminatorType.STRING
+)
 public class Vehicle {
 
     @Id
