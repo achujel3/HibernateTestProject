f665f022c065784b99f43100b8fd38da0427eba8
achujelas123 <achujel3@gmail.com>
2017-04-10 11:14:13 +0300

    testing criteria API

---
+++ src/com/hibernate/test/experiments/CriteriaApi.java
@@ -0,0 +1,50 @@
+package com.hibernate.test.experiments;
+
+import com.hibernate.test.user.UserDetails;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class CriteriaApi {
+
+
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        // Create CriteriaBuilder
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+
+        // Create CriteriaQuery
+        CriteriaQuery<UserDetails> criteria = builder.createQuery(UserDetails.class);
+
+        Root<UserDetails> root = criteria.from(UserDetails.class);
+
+//        Criteria criteria = session.createCriteria(UserDetails.class);
+
+
+        Predicate conjunction = builder.conjunction();
+
+        criteria.where(builder.and(builder.like(root.get("username"), "Username 10")));
+
+        Query query = session.createQuery(criteria);
+
+        List<UserDetails> users = query.getResultList();
+
+        session.getTransaction().commit();
+        session.close();
+
+        for (UserDetails u : users)
+            System.out.println(u.toString());
+    }
+
+
+}
