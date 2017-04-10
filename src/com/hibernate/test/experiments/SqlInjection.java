28480ef2b8ca03d8640c1c49f2e08e5843a70e9e
achujelas123 <achujel3@gmail.com>
2017-04-10 09:33:01 +0300

    testing parameter binding and SQJ injection

---
+++ src/com/hibernate/test/experiments/SqlInjection.java
@@ -0,0 +1,37 @@
+package com.hibernate.test.experiments;
+
+import com.hibernate.test.user.UserDetails;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class SqlInjection {
+
+    public static void main(String[] args) {
+
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        int minUserId = 5;
+        String username = "Username 10";
+
+        Query query = session.createQuery("from USER_DETAILS where userId > :minUserId and username = :username");
+        query.setParameter("minUserId", minUserId);
+        query.setParameter("username", username);
+        List<UserDetails> users = query.list();
+
+
+        session.getTransaction().commit();
+        session.close();
+
+        for (UserDetails user : users) {
+            System.out.println(user.toString());
+        }
+
+    }
+
+}
